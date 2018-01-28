dbba679d19291bf0abd478c9261b8194616f0062
Naudé Cruywagen <naude@glucode.com>
2018-01-28 09:50:08 +0200

    Added test for "Once the sell by date has passed, Quality degrades twice as fast"

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -3,16 +3,10 @@ import XCTest
 
 class GildedRoseTests: XCTestCase {
 
-    func testFoo() {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items);
-        app.updateQuality();
-        XCTAssertEqual("fixme", app.items[0].name);
-    }
-
-    static var allTests : [(String, (GildedRoseTests) -> () throws -> Void)] {
-        return [
-            ("testFoo", testFoo),
-        ]
+    func testThatQualityDegradesTwiceAsFastAfterSellByDateHasPasses() {
+        let item = Item(name: "Item", sellIn: 0, quality: 6)
+        let gildedRose = GildedRose(items: [item])
+        gildedRose.updateQuality()
+        XCTAssertEqual(item.quality, 4)
     }
 }
