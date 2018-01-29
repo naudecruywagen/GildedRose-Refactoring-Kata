fffc0ca0fb8481291ff452f80505bc1759869d4a
Naudé Cruywagen <naude@glucode.com>
2018-01-29 08:28:24 +0200

    Extracted constants and updateQuality(Item:) func

--- swift/Sources/GildedRose.swift
+++ swift/Sources/GildedRose.swift
@@ -1,5 +1,10 @@
 
 public class GildedRose {
+    
+    static let AgedBrie = "Aged Brie"
+    static let BackstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+    static let Sulfuras = "Sulfuras, Hand of Ragnaros"
+    
     var items:[Item]
     
     required public init(items:[Item]) {
@@ -9,50 +14,56 @@ public class GildedRose {
     public func updateQuality() {
         
         for i in 0..<items.count {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+            guard let item = items[i] as? Item  else { return }
+            
+            updateQuality(item: item)
+            
+            if (item.name != GildedRose.Sulfuras) {
+                item.sellIn = item.sellIn - 1
             }
             
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != GildedRose.AgedBrie) {
+                    if (item.name != GildedRose.BackstagePasses) {
+                        if (item.quality > 0) {
+                            if (item.name != GildedRose.Sulfuras) {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+        }
+    }
+    
+    func updateQuality(item: Item) {
+        if (item.name != GildedRose.AgedBrie && item.name != GildedRose.BackstagePasses) {
+            if (item.quality > 0) {
+                if (item.name != GildedRose.Sulfuras) {
+                    item.quality = item.quality - 1
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+                
+                if (item.name == GildedRose.BackstagePasses) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                    
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
                     }
                 }
             }
