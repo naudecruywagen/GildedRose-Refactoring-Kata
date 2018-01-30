b8cb858ec0f8ccaef5ee9e7656d73325dfa817c1
Naudé Cruywagen <naude@glucode.com>
2018-01-30 08:25:53 +0200

    Made BackstagePassesItemUpdater a subclass of ItemUpdater

--- swift/Sources/BackstagePassesItemUpdater.swift
+++ swift/Sources/BackstagePassesItemUpdater.swift
@@ -1,43 +1,30 @@
 import Foundation
 
-class BackstagePassesItemUpdater {
-    private var item: Item
+class BackstagePassesItemUpdater: ItemUpdater {
 
-    init(item: Item) {
-        self.item = item
-    }
-
-    func updateItem() {
-        updateQuality(item: item)
-        decreaseSellIn(item: item)
-        updateQualityIfExpired(item: item)
-    }
-
-    func updateQuality(item: Item) {
+    override func updateQuality(item: Item) {
         if (item.quality < 50) {
             item.quality = item.quality + 1
 
-            if (item.name == GildedRose.BackstagePasses) {
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
                 }
+            }
 
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
                 }
             }
         }
     }
 
-    func decreaseSellIn(item: Item) {
+    override func decreaseSellIn(item: Item) {
         item.sellIn = item.sellIn - 1
     }
 
-    func updateQualityIfExpired(item: Item) {
+    override func updateQualityIfExpired(item: Item) {
         if (item.sellIn < 0) {
             item.quality = item.quality - item.quality
         }
