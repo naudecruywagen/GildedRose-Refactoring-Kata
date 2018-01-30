2dfd1ed11a403f50efa84813a6b006e25d0dac8a
Naudé Cruywagen <naude@glucode.com>
2018-01-30 08:27:09 +0200

    Used ItemUpdater as the default

--- swift/Sources/GildedRose.swift
+++ swift/Sources/GildedRose.swift
@@ -25,27 +25,8 @@ public class GildedRose {
                 let sulfurasItemUpdater = SulfurasItemUpdater(item: item)
                 sulfurasItemUpdater.updateItem()
             default:
-                updateQuality(item: item)
-                decreaseSellIn(item: item)
-                updateQualityIfExpired(item: item)
-            }
-        }
-    }
-
-    func updateQuality(item: Item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1
-        }
-    }
-
-    func decreaseSellIn(item: Item) {
-        item.sellIn = item.sellIn - 1
-    }
-
-    func updateQualityIfExpired(item: Item) {
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1
+                let itemUpdater = ItemUpdater(item:item)
+                itemUpdater.updateItem()
             }
         }
     }
