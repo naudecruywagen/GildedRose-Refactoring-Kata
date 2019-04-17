9f6e3f798bd40b932351e44c1b658b3da213cbc3
Naudé Cruywagen <naude@overhq.com>
2019-04-17 12:13:20 +0200

    Created framework instead of cli target

---
+++ swift/GildedRose/GildedRose.h
@@ -0,0 +1,18 @@
+//
+//  GildedRose.h
+//  GildedRose
+//
+//  Created by Naudé Cruywagen on 2019/04/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GildedRose.
+FOUNDATION_EXPORT double GildedRoseVersionNumber;
+
+//! Project version string for GildedRose.
+FOUNDATION_EXPORT const unsigned char GildedRoseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GildedRose/PublicHeader.h>
+
+
